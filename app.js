d22f9cde82a900f53e3851133adde5c9ba0ee35c
Seba Fox <seba.n.fox@gmail.com>
2024-06-07 09:37:44 -0500

    javascript

--- app.js
+++ app.js
@@ -7,13 +7,44 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+const a = 20;
+const b = 4;
+const add = a + b;
+const minus = a - b;
+const multiply = a * b;
+const dividing = a / b;
+console.log(add, minus, multiply, dividing);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
-
+let num =11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName ="Frodo";
+let lastName = "Baggins";
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName +num);
+console.log(isPresent + str);
+console.log(firstName + lastname);
 // YOUR CODE HERE
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
 
+console.log(val == str3);
+console.log(val === str3);
+console.log(!isPresent2);
+console.log("eleven" == str4 && val >= str3);
+console.log(!isPresent2 || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
 // YOUR CODE HERE
